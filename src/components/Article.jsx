cc77ae548d358710ef707c76ae37dfc3a6ccccd3
schnee25 <schnee.2114@gmail.com>
2021-11-29 15:36:10 +0900

    learn useState

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,25 @@
-import React from "react";
-import { Content, Title } from "./index"; // 名前付きインポート
+import React, { useState } from "react";
+import { Content, Title, PublishButton } from "./index"; // 名前付きインポート
+
 const Article = (props) => {
+  const [isPublished, setIsPublished] = useState(false);
+  console.log(isPublished);
+  const publishArticle = () => {
+    setIsPublished(true);
+  };
   return (
     <div>
       {" "}
       <Title title={props.title} />
       <Content content={props.content} />
+      <PublishButton isPublished={isPublished} onClick={publishArticle} />
+      {/* 
+      onClick={publishArticle}
+      onClick={() => publishArticle()}
+      はおkだけど
+      onClick={publishArticle()}
+      はダメ (無限レンダリングが起きるから)
+      */}
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Article } from "./Article";
 export { default as Content } from "./Content";
 export { default as Title } from "./Title";
+export { default as PublishButton } from "./PublishButton";
